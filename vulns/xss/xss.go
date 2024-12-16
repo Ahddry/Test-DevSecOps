b66bfa4e102599f4f812fd96dfd6564f55ddd9c9
Ahddry <blair.adrien@outlook.com>
2024-12-16 17:12:22 +0100

    feat: change vulns

--- vulns/xss/xss.go
+++ vulns/xss/xss.go
@@ -54,53 +54,14 @@ func xss1Handler(w http.ResponseWriter, r *http.Request, _ httprouter.Params){
 
 		if term == ""{
 			data["term"] = ""
-		}else if vulnDetails == ""{
+		}else
 			data["value"] = template.HTML(value)
 			data["term"] = template.HTML(notFound) //vulnerable function
-		}else{
-			vuln := fmt.Sprintf("<b>%s</b>",term)
-			data["value"] = template.HTML(value)
-			data["term"] = template.HTML(vuln)
-			data["details"] = vulnDetails
 		}
 
 	}
 	data["title"] = "Cross Site Scripting"
 	util.SafeRender(w,r, "template.xss1", data)
-}
-
-func xss2Handler(w http.ResponseWriter, r *http.Request, _ httprouter.Params){
-
-	uid := r.FormValue("uid")
-
-	if(util.CheckLevel(r)){ //level = high
-		uid = HTMLEscapeString(uid)
-	}
-
-	p := sqli.NewProfile() //using sqli get profile module instead of create new function
-	err := p.SafeQueryGetData(uid)
-
-	if err != nil{
-		log.Println(err.Error())
-	}
-
-	data := make(map[string]interface{})
-
-	js := ` <script>
-			var id = %s
-			var name = "%s"
-			var city = "%s"
-			var number = "%s"
-			</script>` //here is the mistake, render value to a javascript that came from client request
-
-	inlineJS := fmt.Sprintf(js,uid, p.Name, p.City, p.PhoneNumber)
-
-	data["title"] = "Cross Site Scripting"
-
-	data["inlineJS"] = template.HTML(inlineJS) //this will render the javascript on client browser
-
-	util.SafeRender(w, r, "template.xss2", data)
-
 }
 
 func HTMLEscapeString(text string)string{
